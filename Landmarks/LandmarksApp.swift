3b20ef40c6f433359cd4c1aa94852cb9c11caead
rlaalswhd <rlaalswhd200@naver.com>
2023-05-15 15:01:08 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 김민종 on 2023/05/15.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
